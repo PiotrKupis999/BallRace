35ce4c85ed0bb3a17dfbc9873e1e2bf7f3aabc19
PiotrKupis999 <piotr.kupis999@gmail.com>
2023-03-14 22:02:41 +0100

    git

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
 
     public GameObject starting_wheel;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    public TextMeshProUGUI text;
+
+
+    private void Awake()
+    {
+        Instantiate(starting_wheel);
+    }
 
     // Start is called before the first frame update
     void Start()
